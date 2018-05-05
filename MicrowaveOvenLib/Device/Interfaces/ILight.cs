9f49d7664996c169145c2c32b86906cd1b86dd4c
wczapla <wojtek.czapla@gmail.com>
2018-05-05 07:43:51 +0200

    ignored list change + unnecessary files remove

--- MicrowaveOvenLib/Device/Interfaces/ILight.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicrowaveOvenLib.Device.Interfaces
-{
-    public interface ILight
-    {
-        bool LightOn { get; }
-        void On();
-        void Off();
-    }
-}
--- MicrowaveOvenLib/Hardware/Interfaces/IUserInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicrowaveOvenLib.Hardware.Interfaces
-{
-    public interface IUserInterface
-    {
-        void DoorOpen();
-        void DoorClose();
-        void PressButtonStart();
-    }
-}
