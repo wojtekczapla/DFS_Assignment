a1ae3166508c93240576a6194614c24ea92bbe66
wczapla <wojtek.czapla@gmail.com>
2018-07-18 23:15:20 +0200

    timer change

--- MicrowaveOvenLib/Hardware/HeatingTimer.cs
+++ MicrowaveOvenLib/Hardware/HeatingTimer.cs
@@ -43,7 +43,8 @@
 
         protected virtual void OnTimeElapsed()
         {
-            this.TimeElapsed?.Invoke(this, null);
+            if(this.TimeElapsed != null)
+                this.TimeElapsed.Invoke(this, null);
         }
 
         private void TimerElapsed(object state)
